3583e80250b49e09a91a9233b90fbfcda5012598
Nutthakarnmild <139094611+Nutthakarnmild@users.noreply.github.com>
2023-07-16 20:39:18 +0700

    Add files via upload

---
+++ home work.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{	cout<<"    c    "<<endl;
+	cout<<"   c c    "<<endl;
+	cout<<"  c   c    "<<endl;
+	cout<<" c     c    "<<endl;
+	cout<<" ccccccc    "<<endl;
+	cout<<" c     c    "<<endl;
+}
