ae2da850cd4f878fd2a1a0d5b831772117a5d473
Nutthakarnmild <139094611+Nutthakarnmild@users.noreply.github.com>
2023-07-24 10:08:00 +0700

    Update score.cpp

--- score.cpp
+++ score.cpp
@@ -6,7 +6,7 @@ int main()
 	float s;
 	cout << "Enter score : ";
 	cin >> s;
-	cout << "You "<< ((s>=50)? "Pass":"Faill") << " with score" << s <<endl; 
+	cout << "You "<< ((s>=50)? "Pass":"Fail") << " with score" << s <<endl; 
 	system("pause");
 	return(0);
 }
