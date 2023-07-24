6b378e167676b85aa53de6e7a183e3a40356326d
Nutthakarnmild <139094611+Nutthakarnmild@users.noreply.github.com>
2023-07-24 09:57:42 +0700

    Add files via upload

---
+++ score.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	float s;
+	cout << "Enter score : ";
+	cin >> s;
+	cout << "You "<< ((s>=50)? "Pass":"Faill") << " with score" << s <<endl; 
+	system("pause");
+	return(0);
+}
