c7b6a4e0713af8352b3ab48d90316393c6ffdc89
Nutthakarnmild <139094611+Nutthakarnmild@users.noreply.github.com>
2023-08-06 23:58:55 +0700

    Add files via upload

---
+++ Character.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+int main()
+{	
+	int A=10,B=20,C=30;
+	int t = 0;
+	char a,b,c;
+	cout<<"Enter Character (A,B,C) : ";
+	cin>>a;
+	if (a == 'A')
+	{	t +=10 ;
+		A +=10;
+	}
+		
+	else if (a == 'B')
+	{	t+=20;
+		B += 20;
+	}
+	else if (a == 'C')
+	{	t+= 30;
+		C +=30;
+
+	}
+
+	cout << "Enter Character (A,B,C) : " ;
+	cin >> b ;
+	if (b == 'A')
+	{	t +=10 ;
+		A +=10;
+	}
+		
+	else if (b == 'B')
+	{	t+=20;
+		B += 20;
+	}
+	else if (b == 'C')
+	{	t+= 30;
+		C +=30;
+
+	}
+
+	cout << "Enter Character (A,B,C) : " ;
+	cin >> c ;
+	if (c == 'A')
+	{	t +=10 ;
+		A +=10;
+	}
+		
+	else if (c == 'B')
+	{	t+=20;
+		B += 20;
+	}
+	else if (c == 'C')
+	{	t+= 30;
+		C +=30;
+
+	}
+
+	//ex.A = 10 , B = 20 , C = 30
+    cout << ((a == 'A') ? "A = 10" : (a == 'B') ? "B = 20" : "C = 30") << " , ";
+    cout << ((b == 'A') ? "A = 10" : (b == 'B') ? "B = 20" : "C = 30") << " , ";
+    cout << ((c == 'A') ? "A = 10" : (c == 'B') ? "B = 20" : "C = 30") << endl;
+    
+	//ex. A + A + A =
+	cout << ((a == 'A') ? "A" : (a == 'B') ? "B" : "C") << " + ";
+    cout << ((b == 'A') ? "A" : (b == 'B') ? "B" : "C") << " + ";
+    cout << ((c == 'A') ? "A" : (c == 'B') ? "B" : "C") << " = ";
+
+	//ex. 10 + 10 + 10 =3
+	cout << ((a == 'A') ? "10" : (a == 'B') ? "20" : "30") << " + ";
+    cout << ((b == 'A') ? "10" : (b == 'B') ? "20" : "30") << " + ";
+    cout << ((c == 'A') ? "10" : (c == 'B') ? "20" : "30") << " = " << t << endl;
+	
+	return(0);
+}
