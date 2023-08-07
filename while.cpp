879f667774f381318543f24b156f50be7ba3d8e2
Nutthakarnmild <139094611+Nutthakarnmild@users.noreply.github.com>
2023-08-07 11:42:25 +0700

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	/*int num,i = 1,value,neg = 0,pos = 0,zero = 0;
+	cout<<"Enter number : ";
+	cin>>num;
+	while(i <= num)
+	{
+		cout<<"Enter value"<<i<<" : ";
+		cin>>value;
+		i++;
+		if (value < 0)neg++;
+		else if (value > 0)pos++;
+		else if (value == 0)zero++;
+			
+	}
+		cout<<endl;
+		cout<<"Negative = "<< neg <<endl;
+		cout<<"Positive = "<< pos <<endl;
+		cout<<"Zero = "<< zero <<endl;
+	int num,value,sum;
+	bool Flag = true;
+	num = 1;
+	sum = 0;
+	do{
+		cout<<"Enter value (1-20) "<< num <<" : ";
+		cin>>value;
+		if ((value >= 0)&&(value <= 20)){
+		sum += value;
+			num++;
+		{
+		else Flag = false;
+		}while(Flag == true);
+		if (num > 1){
+			float Agv;
+			Agv = static
+	}*/
+	
+
+
+	char Ch;
+	int Count = 0;
+	string Str;
+	cout<<"Enter String : ";
+	while (cin >> Ch){
+	Count++;
+	Str+=Ch;
+	}
+	cout<<"\nLength of string("<<Str<<")is";
+	cout<<Count<<endl;
+	system("Pause");
+	return(0);
+}
